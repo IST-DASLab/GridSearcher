a03ce1658bcf4884e846823a27691b30f6729b58
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2025-11-13 15:33:17 +0100

    expose SchedulingConfig and TorchRunConfig

--- gridsearcher/__init__.py
+++ gridsearcher/__init__.py
@@ -1,10 +1,13 @@
 from .sbatch import SBATCH
 from .gridsearcher import GridSearcher
 from .tools import GSExe, GSKeyValSep
+from .configs import SchedulingConfig, TorchRunConfig
 
 __all__ = [
     'SBATCH',
     'GridSearcher',
     'GSExe',
-    'GSKeyValSep'
+    'GSKeyValSep',
+    'SchedulingConfig',
+    'TorchRunConfig',
 ]
